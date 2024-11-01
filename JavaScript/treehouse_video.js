9d93e226e2497ad8bdcdeeacdca3dc5a2934f588
Bryce Thomas <brycet4477@outlook.com>
2024-11-01 16:02:15 -0400

    update

--- JavaScript/treehouse_video.js
+++ JavaScript/treehouse_video.js
@@ -1,1 +1,3 @@
-
+var name = "Bryce";
+alert('Hi, ${name}. Want to see something cool?');
+document.querySelector('html').innerHTML = '<h1>Welcome to ${name}s site!</h1> <img src="https://media.giphy.com/media/JIX9t2j0ZTN9S/giphy.gif">';
