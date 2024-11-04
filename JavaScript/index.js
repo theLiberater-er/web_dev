9252fa542c3008361d67c20fe39afdac52d32954
Bryce Thomas <brycet4477@outlook.com>
2024-11-04 16:05:07 -0500

    Add files via upload

--- JavaScript/index.js
+++ JavaScript/index.js
@@ -1,1 +1,31 @@
-console.log("Hello World!");
+let myNumber = 42;
+
+let myFloat = 42.0;
+
+console.log(String(myNumber) + myFloat);
+
+
+/*let word = "Jabroni";
+
+switch (word) {
+  case 'Jabroni':
+    alert("Patron Tequila");
+	break;
+  case 'School Counselor':
+    alert("Anything with Alcohol");
+	break;
+  case 'Programmer':
+    alert("Hipster Craft Beer");
+	break;
+  case 'Bike Gang Member':
+	alert("Moonshine");
+	break;
+  case 'Politician':
+	alert("Your tax dollars");
+	break;
+  case 'Rapper':
+	alert("Cristal");
+	break;
+  default:
+    alert("Beer");
+} */
