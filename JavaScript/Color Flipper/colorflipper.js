6a0516f9484c94a1f9c18de312f9ca93e45f3fcd
Bryce Thomas <brycet4477@outlook.com>
2024-11-27 16:20:21 -0500

    Add files via upload

--- JavaScript/Color Flipper/colorflipper.js
+++ JavaScript/Color Flipper/colorflipper.js
@@ -1,16 +1,51 @@
-const colors = ["green", "red", "rgba(133,122,200)", "#f15025"];
-const btn = document.getElementById('#btn');
+const btn = document.getElementById('btn');
+const btn_white = document.getElementById('btn_white');
+const color = document.body.style.backgroundColor;
+const header = document.getElementById('myHeader');
+
+function theme () {
+	if (color === 'white') {
+	document.getElementById("myHeader").style.color = "white";
+	color.append("black"); //document.body.style.backgroundColor = "black";
+}
+ else {
+	 document.getElementById("myHeader").style.color = "black"
+	 document.body.style.backgroundColor = 'white';
+}
+
+console.log(color);
+	}
+//function lightMode () {
+	//document.getElementById("myHeader").style.color = "black"
+	//document.body.style.backgroundColor = 'white';
+	
+//}
+
+btn.addEventListener('click', function () {
+	theme();
+});
+
+console.log(color);
+
+
+
+
+
+
+/*const colors = ["green", "red", "rgba(133,122,200)", "#f15025"];
+const btn = document.getElementById("btn");
 const color = document.querySelector('.color');
 
-btn.addEventListener("click", function() {
+function getRandomNumber(){
+	return Math.random();
+}
+
+btn.addEventListener("click", function () {
 	//Get random number btw 0 and 3;
 	const randomNumber = getRandomNumber();
 	
 	document.body.style.backgroundColor = colors[randomNumber];
 	color.textContent = colors[randomNumber];
 	
-});
+}); */
 
-function getRandomNumber(){
-	return Math.random();
-}
